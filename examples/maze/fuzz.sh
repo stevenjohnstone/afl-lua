afa855b75db93f3fdca9c063971d57bc4a2dd44a
Stevie Johnstone <steven.james.johnstone@gmail.com>
2020-08-10 19:00:02 +0100

    Improve example performance with "FidgetyAFL"

--- examples/maze/fuzz.sh
+++ examples/maze/fuzz.sh
@@ -3,7 +3,7 @@ mkdir -p {in,out}
 echo -n "s" > in/first
 
 start=$(date +%s.%N)
-LUA_CPATH="../../?.so" AFL_BENCH_UNTIL_CRASH=1 afl-fuzz -i in -o out ../../afl-lua mazefuzz.lua
+LUA_CPATH="../../?.so" AFL_BENCH_UNTIL_CRASH=1 afl-fuzz -i in -o out -d ../../afl-lua mazefuzz.lua
 duration=$(echo "$(date +%s.%N) - $start" | bc)
 
 for crash in out/crashes/id*; do
